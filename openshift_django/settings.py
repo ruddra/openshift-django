180812679554f9697f33c3bc63f95007e1bf98f8
Arnab Kumar Shil <arnab.shil@nhst.com>
2018-07-23 13:27:54 +0600

    updated

--- openshift_django/settings.py
+++ openshift_django/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydjango',
-        'USER': 'myroot',
-        'PASSWORD': 'myroot123',
+        'USER': 'root',
+        'PASSWORD': 'root123',
         'HOST': '172.30.0.30',  # Your openshift host
         'PORT': '3306',
     }
