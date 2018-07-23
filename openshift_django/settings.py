aa262cc50daa3700fb70ffa7782ee42edf6b288b
Arnab Kumar Shil <arnab.shil@nhst.com>
2018-07-23 13:56:15 +0600

    updated

--- openshift_django/settings.py
+++ openshift_django/settings.py
@@ -138,5 +138,5 @@ NOSE_ARGS = [
     '--cover-package=openshift-django',
    '--verbosity=2',  # verbose output
     '--with-xunit',  # enable XUnit plugin
-    '--xunit-file=xunittest.xml',  # the XUnit report file
+    '--xunit-file=/tmp/xunittest.xml',  # the XUnit report file
 ]
