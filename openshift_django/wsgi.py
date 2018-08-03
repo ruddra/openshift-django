cfb9bea68454dc17192e5cd20d810d357b85431a
Arnab Kumar Shil <arnabkumarshil@Arnabs-MacBook-Pro.local>
2018-08-03 23:50:59 +0600

    Updated

--- openshift_django/wsgi.py
+++ openshift_django/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openshift_django.settings")
 
 application = get_wsgi_application()
